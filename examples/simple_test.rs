0018013434ab3508f19f359aac478af0a8c2bb8c
Helly Guo <hellyguo@foxmail.com>
2026-04-29 01:22:59 +0800

    fix: add complete workbook structure (BrtFileVersion, BrtWbProp, BrtBookView) and use XLWideString format (4-byte length)

---
+++ examples/simple_test.rs
@@ -0,0 +1,10 @@
+use rxlsb::{XlsbWriter, CellData};
+use std::path::PathBuf;
+
+fn main() {
+    let path = PathBuf::from("/tmp/simple_rxlsb.xlsb");
+    let mut writer = XlsbWriter::builder().path(&path).build().unwrap();
+    writer.write_batch("Test", |_row, _col| CellData::number(123.45), 3, 2).unwrap();
+    writer.close().unwrap();
+    println!("Created rxlsb: {}", path.display());
+}
--- src/format/workbook_writer.rs
+++ src/format/workbook_writer.rs
@@ -27,22 +27,42 @@ impl WorkbookWriter {
         writer.write_varint(RecordType::BrtBeginBook.to_u32());
         writer.write_varsize(0);
         
+        self.write_brt_file_version(&mut writer);
+        self.write_brt_wb_prop(&mut writer);
+        
+        writer.write_varint(RecordType::BrtBeginBookViews.to_u32());
+        writer.write_varsize(0);
+        
+        self.write_brt_book_view(&mut writer);
+        
+        writer.write_varint(RecordType::BrtEndBookViews.to_u32());
+        writer.write_varsize(0);
+        
         writer.write_varint(RecordType::BrtBeginBundleShs.to_u32());
         writer.write_varsize(0);
         
         for (i, sheet_name) in self.sheets.iter().enumerate() {
             let rel_id = format!("rId{}", i + 1);
-            let rel_id_size = BufferWriter::utf16le_byte_length(&rel_id) 
-                + BufferWriter::varint_size(rel_id.encode_utf16().count() as u32);
-            let name_size = BufferWriter::utf16le_byte_length(sheet_name) 
-                + BufferWriter::varint_size(sheet_name.encode_utf16().count() as u32);
+            let rel_id_chars = rel_id.encode_utf16().count();
+            let name_chars = sheet_name.encode_utf16().count();
+            
+            let rel_id_size = 4 + rel_id_chars * 2;
+            let name_size = 4 + name_chars * 2;
             
             writer.write_varint(RecordType::BrtBundleSh.to_u32());
             writer.write_varsize((8 + rel_id_size + name_size) as u32);
             writer.write_u32_le(0);
             writer.write_u32_le(i as u32 + 1);
-            writer.write_wide_string(&rel_id);
-            writer.write_wide_string(sheet_name);
+            
+            writer.write_u32_le(rel_id_chars as u32);
+            for ch in rel_id.encode_utf16() {
+                writer.write_u16_le(ch);
+            }
+            
+            writer.write_u32_le(name_chars as u32);
+            for ch in sheet_name.encode_utf16() {
+                writer.write_u16_le(ch);
+            }
         }
         
         writer.write_varint(RecordType::BrtEndBundleShs.to_u32());
@@ -52,5 +72,38 @@ impl WorkbookWriter {
         writer.write_varsize(0);
         
         Ok(writer.freeze())
+    }
+    
+    fn write_brt_file_version(&self, writer: &mut BufferWriter) {
+        writer.write_varint(128);
+        writer.write_varsize(44);
+        writer.write_bytes(&[
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00,
+            0x00, 0x00, 0x78, 0x00, 0x6c, 0x00, 0x01, 0x00,
+            0x00, 0x00, 0x33, 0x00, 0x01, 0x00, 0x00, 0x00,
+            0x35, 0x00, 0x04, 0x00, 0x00, 0x00, 0x39, 0x00,
+            0x33, 0x00, 0x30, 0x00, 0x32, 0x00, 0x00
+        ]);
+    }
+    
+    fn write_brt_wb_prop(&self, writer: &mut BufferWriter) {
+        writer.write_varint(153);
+        writer.write_varsize(12);
+        writer.write_bytes(&[
+            0x20, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00
+        ]);
+    }
+    
+    fn write_brt_book_view(&self, writer: &mut BufferWriter) {
+        writer.write_varint(158);
+        writer.write_varsize(29);
+        writer.write_bytes(&[
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x80, 0x70, 0x00, 0x00, 0xcf, 0x30, 0x00, 0x00,
+            0x58, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x78
+        ]);
     }
 }
